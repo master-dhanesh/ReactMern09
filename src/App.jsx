d4fce8c2e384d6741e6a0a0c7877560df567a656
master-dhanesh <dhanesh1296@gmail.com>
2024-07-07 15:29:17 +0530

    Rendering List | CSS Implementation | tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,110 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import "./App.css";
+import css from "./App.module.css";
 
 const App = () => {
-    const [username, setusername] = useState("");
-    const [names, setnames] = useState([]);
+    const [title, settitle] = useState("");
+    const [status, setstatus] = useState("incomplete");
+    const [description, setdescription] = useState("");
 
-    // const SubmitHandler = (e) => {
-    //     e.preventDefault();
-    //     console.log(e.target.name.value);
-    // };
+    const [tasks, settasks] = useState([
+        {
+            title: "title 1",
+            description: "A normal description",
+            status: "incomplete",
+        },
+    ]);
 
-    const SubmitHandler = (e) => {
+    const submithandler = (e) => {
         e.preventDefault();
+        const newtask = { title, status, description };
 
-        const copynames = [...names];
-        copynames.push(username);
-        setnames(copynames);
+        const copytasks = [...tasks];
+        copytasks.push(newtask);
+        settasks(copytasks);
 
-        // setnames([...names, username]);
-
-        setusername("");
+        settitle("");
+        setstatus("incomplete");
+        setdescription("");
     };
 
-    console.log(names);
+    const mycss = {
+        color: "royalblue",
+    };
+
     return (
-        <div>
-            <form onSubmit={SubmitHandler}>
-                {/* <input type="text" placeholder="name" name="name" /> */}
+        <div className="w-[80%] mt-10 mx-auto ">
+            <h1 className="typo mb-10 text-3xl font-extrabold">
+                Two Way Binding | Rendering
+            </h1>
+            <form
+                onSubmit={submithandler}
+                className=" mx-auto bg-zinc-200 p-5 "
+            >
                 <input
                     type="text"
-                    placeholder="name"
-                    onChange={(e) => setusername(e.target.value)}
-                    value={username}
+                    className="mb-5 block rounded-lg p-3 border w-1/2"
+                    placeholder="Title"
+                    onChange={(e) => settitle(e.target.value)}
+                    value={title}
                 />
-                <button>Submit</button>
+                <label htmlFor="" className="block mb-5">
+                    <input
+                        name="status"
+                        type="radio"
+                        placeholder="Title"
+                        onChange={(e) => setstatus("complete")}
+                        checked={status === "complete" ? true : false}
+                    />
+                    Complete
+                    <input
+                        type="radio"
+                        name="status"
+                        className="ml-5"
+                        placeholder="Title"
+                        onChange={(e) => setstatus("incomplete")}
+                        checked={status === "incomplete" ? true : false}
+                    />
+                    InComplete
+                </label>
+
+                <textarea
+                    type="text"
+                    className="mb-5 block rounded-lg p-3 border w-1/2"
+                    placeholder="Enter Description here..."
+                    onChange={(e) => setdescription(e.target.value)}
+                    value={description}
+                ></textarea>
+
+                <button className="rounded px-5 py-2 text-white bg-black">
+                    Submit
+                </button>
             </form>
+            <hr />
+            <h1 className={`${css.typoWord} mb-10 text-3xl font-extrabold`}>
+                Pending Tasks
+            </h1>
+
+            <ul className="list-decimal">
+                {tasks.map((t, i) => {
+                    return (
+                        <li
+                            key={i}
+                            className="rounded mb-3 p-2 w-[25%] flex justify-between bg-zinc-200"
+                        >
+                            <span style={{ color: "tomato", fontSize: "28px" }}>
+                                {t.title}
+                            </span>
+                            <span className="text-xl flex gap-x-3">
+                                <i style={mycss} className="ri-pencil-line"></i>
+                                <i className="ri-close-fill"></i>
+                            </span>
+                        </li>
+                    );
+                })}
+            </ul>
         </div>
     );
 };
 
 export default App;
-
-// [{
-//     title: "",
-//     description: "",
-//     status: ""
-// }]
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "remixicon/fonts/remixicon.css";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <div>
+        <h1 className="text-3xl typo">Dummy</h1>
+        <App />
+    </div>
+);
